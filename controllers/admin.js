6e4383bc8a57333a06a5012b32e4632aadc254f7
pankaj-12-debug <kajopkkumar12@gmail.com>
2023-03-27 20:27:55 +0530

    update changes

--- controllers/admin.js
+++ controllers/admin.js
@@ -110,13 +110,19 @@ exports.getProducts = (req, res, next) => {
     console.log(err);
   })
 };
-/*
+
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
  // Product.deleteById(prodId);
- Product.findByPk(prodId).then(product=>{
-  return product.destroy();
- }).then(result=>{
+ Product.deleteById(prodId)
+ //.then(()=>{
+  //return product.destroy();
+ //})
+ .then(()=>{
   console.log('delete product');
   res.redirect('/admin/products');
-}; */
+ })
+ .catch(err=>{
+  console.log(err);
+ })
+}; 
--- models/product.js
+++ models/product.js
@@ -9,7 +9,7 @@ class Product{
     this.price=price,
     this.description=description,
     this.imageUrl=imageUrl
-    this._id=new mongodb.ObjectId(id)
+    this._id=id ? new mongodb.ObjectId(id):null;
   }
   save()
   {
@@ -59,6 +59,16 @@ else{
     .catch(err=>{
       console.log(err)
     })
+  }
+  static deleteById(prodId)
+  {
+    const db=getDb();
+   return db.collection('products').deleteOne({_id:new mongodb.ObjectId(prodId)})
+   .then(result=>{
+    console.log('Deleted')
+   }).catch(err=>{
+    console.log(err);
+   })
   }
 }
 /*const Product=sequelize.define('product',{
